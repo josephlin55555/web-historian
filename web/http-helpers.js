4acc66a282e02ad5b144348f6f8c031b824db00a
josephlin55555 <josephlin55555@gmail.com>
2015-04-15 16:17:28 -0700

    Finished basic REST, returns basic index and archived sites

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -13,8 +13,18 @@ exports.headers = headers = {
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  var target = asset || 'web/public/index.html';
+  if(asset){
+    target = "archives/sites/" + target;
+  }
+
+
+  fs.readFile(target, 'utf-8', function(error, data) {
+    if(error) {
+      throw error;
+    }
+    res.end(data);
+  });
 };
-
-
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,37 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var hHelpers = require('./http-helpers.js')
 // require more modules/folders here!
 
+var sendResponse = function(req, res, statusCode){
+  statusCode = statusCode || 200;
+  res.writeHead(statusCode, hHelpers.headers);
+  var truncatedUrl = req.url.substring(1);
+  hHelpers.serveAssets(res, truncatedUrl);
+
+};
+
+
+
+var routing = {
+  'GET': function(req, res){
+    sendResponse(req, res);
+  },
+  'POST':function(req, res){
+    sendResponse(req, res);
+  },
+  'OPTIONS':function(req,res){
+    sendResponse(req, res);
+  },
+};
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = routing[req.method];
+  if(action){
+  //   console.log('REQUEST HANDLER CAUGHT ACTION')
+  //   console.log(res)
+    action(req, res);
+  } else {
+    sendResponse(res, '404 String Text', 404);
+  }
+  // res.end(archive.paths.list);
 };
